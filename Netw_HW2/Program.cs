b8a7cf4dd55479aab3598a52920baf5d620705ee
NIdeaForNickname <lazavicbagdan@gmail.com>
2025-05-26 08:42:08 +0300

    Created

---
+++ Netw_HW2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    
+    private const int DEFAULT_BUFLEN = 512;
+    private const string DEFAULT_PORT = "27015";
+
+    static async Task Main()
+    {
+        Console.Title = "CLIENT SIDE";
+        try
+        {
+            var ipAddress = IPAddress.Loopback;
+            var remoteEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
+
+            var clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            await clientSocket.ConnectAsync(remoteEndPoint);
+            Console.WriteLine("Нажмите на соответствующую клавишу");
+            Console.Write("[1] - Дата\n[2] - Время\n[3] - Погода\n[4] - Курс Евро\n[5] - Курс Биткоина\n[_] - Случайное число");
+
+            var sendingTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var message = Console.ReadKey(true);
+                    if (!int.TryParse(message.KeyChar.ToString(), out _)) break;
+
+                    byte[] messageBytes = Encoding.UTF8.GetBytes(message.KeyChar.ToString());
+                    await clientSocket.SendAsync(new ArraySegment<byte>(messageBytes), SocketFlags.None);
+                }
+
+                clientSocket.Shutdown(SocketShutdown.Send);
+            });
+
+            var receivingTask = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var buffer = new byte[DEFAULT_BUFLEN];
+                    int bytesReceived = await clientSocket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                    if (bytesReceived > 0)
+                    {
+                        string response = Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                        Console.SetCursorPosition(19, int.Parse(response[0].ToString())+1);
+                        Console.Write($"\t{response.Substring(1)}");
+                        Console.WriteLine("               ");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            });
+
+            await Task.WhenAll(sendingTask, receivingTask);
+
+            clientSocket.Close();
+            Console.WriteLine("Соединение с сервером закрыто.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+}
---
+++ Netw_HW2_serv/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+
+class Server
+{
+    private static Random rnd = new Random();
+    private const int DEFAULT_BUFLEN = 512;
+    private const string DEFAULT_PORT = "27015";
+    private static ConcurrentQueue<(Socket, byte[])> messageQueue = new ConcurrentQueue<(Socket, byte[])>();
+
+    static async Task Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Title = "SERVER SIDE";
+        Console.WriteLine("Процесс сервера запущен!");
+
+        try
+        {
+            var ipAddress = IPAddress.Any;
+            var localEndPoint = new IPEndPoint(ipAddress, int.Parse(DEFAULT_PORT));
+
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint); 
+            listener.Listen(10); 
+            Console.WriteLine("Начинается прослушивание информации от клиента.\nПожалуйста, запустите клиентскую программу!");
+
+            var clientSocket = await listener.AcceptAsync();
+            Console.WriteLine("Подключение с клиентской программой установлено успешно!");
+
+            listener.Close();
+
+            _ = ProcessMessages(); 
+
+            while (true)
+            {
+                var buffer = new byte[DEFAULT_BUFLEN];
+                int bytesReceived = await clientSocket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None); 
+
+                if (bytesReceived > 0)
+                {
+                    messageQueue.Enqueue((clientSocket, buffer));
+                    Console.WriteLine($"Добавлено сообщение в очередь.");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка при получении данных.");
+                    break;
+                }
+            }
+
+            clientSocket.Shutdown(SocketShutdown.Send);
+            clientSocket.Close();
+            Console.WriteLine("Процесс сервера завершает свою работу!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    private static async Task ProcessMessages()
+    {
+        while (true)
+        {
+            if (messageQueue.TryDequeue(out var item))
+            {
+                var (clientSocket, buffer) = item;
+                string message = Encoding.UTF8.GetString(buffer);
+                Console.WriteLine($"Процесс клиента отправил сообщение: {message}");
+                
+                var response = new string("6"+rnd.Next(10));
+                if (int.TryParse(message, out int id))
+                {
+                    var weather = new List<string>{ "Солнечно", "Хмуро", "Дождь", "Ливень", "Снег" };
+                    switch (id)
+                    {
+                        case 1: response = $"{id}{DateTime.Now:yyyy-MM-dd}";break; // ого
+                        case 2: response = $"{id}{DateTime.Now:HH:mm:ss}";break;
+                        case 3: response = $"{id}{weather[rnd.Next(weather.Count)]}";break;
+                        case 4: response = $"{id}{Math.Round(rnd.NextDouble() * 4 + 46, 2)}";break;
+                        case 5: response = $"{id}{Math.Round(rnd.NextDouble() * 40000 + 80000, 2)}";break;
+                    }
+                }
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await clientSocket.SendAsync(new ArraySegment<byte>(responseBytes), SocketFlags.None);
+                Console.WriteLine($"Процесс сервера отправляет ответ: {response}");
+            }
+
+            await Task.Delay(100);  
+        }
+    }
+}
